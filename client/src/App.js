1e42ea12633f16dc8b5a9922913628e03423a633
Tinquen59 <quentand@gmail.com>
2021-07-24 01:32:48 +0200

    add navbar with responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import "./assets/style/style.css"
 import { BrowserRouter as Router } from "react-router-dom";
 import LaRoute from "./route/Router";
 
+import Navbar from "./components/template/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
 
       <LaRoute />
     </Router>
