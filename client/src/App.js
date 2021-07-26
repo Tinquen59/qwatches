d893d1b61ac80754f06b00d59ec4a64538a992f0
Quentin Andrieu <quent@MacBook-Air-de-Quentin.local>
2021-07-26 09:18:21 +0200

    create footer with responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,15 @@ import "./assets/style/style.css"
 import { BrowserRouter as Router } from "react-router-dom";
 import LaRoute from "./route/Router";
 
+import PiedDePage from "./components/PiedDePage";
+
 function App() {
   return (
     <Router>
 
       <LaRoute />
+
+      <PiedDePage />
     </Router>
   );
 }
