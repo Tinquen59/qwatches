55e6c70f9524ba4d453a8e1403ad680d2541a669
Tinquen59 <quentand@gmail.com>
2021-08-19 13:11:11 +0200

    add 'Voir les montres' with responsive

---
+++ client/src/components/Card/CardWatches.jsx
@@ -0,0 +1,16 @@
+export default function CardWatches(props) {
+  const { watchData } = props;
+
+  return (
+    <div className="qa-CardWatch__container">
+        <div className="qa-CardWatch__image">
+            <img src={watchData.image.src} alt={watchData.image.alt} className="qa-CardWatch__image--img" />
+        </div>
+
+        <div className="qa-CardWatch__body">
+            <p className="qa-CardWatch__body--model">{watchData.model}</p>
+            <p className="qa-CardWatch__body--mark">{watchData.mark}</p>
+        </div>
+    </div>
+  );
+}
--- client/src/views/SeeWatches.jsx
+++ client/src/views/SeeWatches.jsx
@@ -1,5 +1,96 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+import CardWatches from "../components/Card/CardWatches";
+
+import swatchImg from "../assets/images/swatch_system51_irony.png";
+import seikoImg from "../assets/images/seiko.jpg";
+
+
 export default function SeeWatches () {
+
+    const dataWatches = [
+        {
+            image: {
+                src: swatchImg,
+                alt: "montre swatch"
+            },
+            model: "Petite seconde black",
+            mark: "Swatch"
+        },
+        {
+            image: {
+                src: seikoImg,
+                alt: "montre seiko"
+            },
+            model: "Aucune idée",
+            mark: "Seiko"
+        },
+        {
+            image: {
+                src: swatchImg,
+                alt: "montre swatch"
+            },
+            model: "Petite seconde black",
+            mark: "Swatch"
+        },
+        {
+            image: {
+                src: seikoImg,
+                alt: "montre seiko"
+            },
+            model: "Aucune idée",
+            mark: "Seiko"
+        },
+        {
+            image: {
+                src: swatchImg,
+                alt: "montre swatch"
+            },
+            model: "Petite seconde black",
+            mark: "Swatch"
+        },
+        {
+            image: {
+                src: seikoImg,
+                alt: "montre seiko"
+            },
+            model: "Aucune idée",
+            mark: "Seiko"
+        }
+    ]
+
     return(
-        <h1>Voir les montres</h1>
+        <>
+            <section className="qa-BasicHero__container qa-BasicHero__container--bgi">
+                <h1 className="qa-BasicHero__container--title">Contempler les montres de collections</h1>
+            </section>
+
+            <section className="qa-SeeWatches__container">
+                <form className="qa-SearchBar__container">
+                    <input
+                        className="qa-SearchBar__container--input"
+                        type="text"
+                        name="search"
+                        placeholder="Rechercher une montre"
+                    />
+                    <button
+                        className="qa-Btn--search"
+                        type="submit"
+                    >
+                        <FontAwesomeIcon
+                            className="qa-SearchBar__container--icon"
+                            icon={faSearch}
+                        />
+                    </button>
+                </form>
+
+                <div className="qa-AllWatches__container">
+                    {dataWatches.map((watch, index) => (
+                        <CardWatches watchData={watch} key={index} />
+                    ))}
+                </div>
+            </section>
+        </>
     );
 }
